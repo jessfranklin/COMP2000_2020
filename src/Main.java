092831134c9f331ce8ffbf9fc9d3703d7b70b594
heyjeka94 <jessica.franklin-da-rocha@students.mq.edu.au>
2020-08-22 19:08:30 -0300

    stage 3, 4, 5

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,43 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("~~~~~~~~~~");
-        System.out.println("Blue vs Red");
-        System.out.println("~~~~~~~~~~");
-    }
-}
+
+    import javax.swing.*;
+    import java.awt.*;
+    
+    class Main extends JFrame {
+    
+        class App extends JPanel {
+    
+            Grid grid;
+    
+            public App() {
+                setPreferredSize(new Dimension(720, 720));
+                grid = new Grid();
+                
+            }
+    
+            @Override
+            public void paint(Graphics g) {
+                grid.paint(g, getMousePosition());
+            }
+    
+        }
+    
+        public static void main(String[] args) throws Exception {
+            System.out.println("Red vs. Blue");
+            Main window = new Main();
+            window.run();
+        }
+    
+        private Main() {
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            App canvas = new App();
+            this.setContentPane(canvas);
+            this.pack();
+            this.setVisible(true);
+        }
+    
+        public void run() {
+            while (true) {
+                this.repaint();
+            }
+        }
+    } 
